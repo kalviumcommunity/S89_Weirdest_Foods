b06bb6229839476deccfb16050c1bff1327d4209
Satya-s89 <kaki.harisatya.s89@kalvium.community>
2025-03-29 10:40:57 +0530

    frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>WORLD WEIRDIEST FOODS</h1>
+      <h2>My project allows users to explore a list of the world's weirdest foods, contributed by users from different regions. Users can share their own unique food discoveries, vote on the most bizarre items, and engage in discussions.</h2>
+    </>
+  )
+}
+
+export default App
