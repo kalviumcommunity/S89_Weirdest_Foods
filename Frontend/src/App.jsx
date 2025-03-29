24bde51881cbc7255b519eb303aa15512e5973d4
Satya-s89 <kaki.harisatya.s89@kalvium.community>
2025-03-29 10:54:21 +0530

    frontend About page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,14 @@
+import { BrowserRouter } from 'react-router-dom'
 import './App.css'
+import Allroutes from './Allroutes'
 
 function App() {
 
   return (
     <>
-      <h1>WORLD WEIRDIEST FOODS</h1>
-      <h2>My project allows users to explore a list of the world's weirdest foods, contributed by users from different regions. Users can share their own unique food discoveries, vote on the most bizarre items, and engage in discussions.</h2>
+      <BrowserRouter>
+        <Allroutes/>
+      </BrowserRouter>
     </>
   )
 }
