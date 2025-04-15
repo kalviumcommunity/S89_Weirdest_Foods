bb9fd868ff7c06e40815f1329da5af7d1b008701
Satya-s89 <kaki.harisatya.s89@kalvium.community>
2025-04-15 14:07:05 +0530

    components

--- Frontend/src/Allroutes.jsx
+++ Frontend/src/Allroutes.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import {Routes,Route} from 'react-router-dom';
 import About from './About';
+import WeirdFood from './components/WierdFood';
 
 const Allroutes = () => {
   return (
     <Routes>
-        <Route path='/' element={<About/>}/>    
+        <Route path='/about' element={<About/>}/>
+        <Route path='/' element={<WeirdFood/>}/>   
     </Routes>
   )
 }
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from 'react-router-dom'
 import './App.css'
 import Allroutes from './Allroutes'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
+        <Navbar/>
         <Allroutes/>
       </BrowserRouter>
     </>
