8e04c27e3bf9ce5f31a34626f4b955c3dfcc5075
Satya-s89 <kaki.harisatya.s89@kalvium.community>
2025-04-19 11:45:44 +0530

    connecting backend and frontend

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,9 @@ const dotenv = require('dotenv');
 const routes = require('./routes');
 dotenv.config();
 
+const cors = require('cors');
+app.use(cors());
+
 const app = express();
 const PORT = process.env.PORT || 8080;
 
